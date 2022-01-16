db130811abf2d22646485d6be069d23cfc3dad80
Christopher Plummer <chrisplummer0194@gmail.com>
2022-01-16 14:11:52 -0500

    Delete role command

---
+++ RoleBot/Commands/DeleteRoleCommand.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Commands;
+
+namespace RoleBot.Commands;
+
+public class DeleteRoleCommand : CommandBase
+{
+    [Command("deleterole")]
+    public async Task HandleCommandAsync([Remainder] string roleName)
+    {
+        var role = Context.Guild.Roles.FirstOrDefault(r => r.Name == roleName);
+        if (!GuildUser.GuildPermissions.Administrator)
+        {
+            await ReplyAsync($"{Mention} Insufficient permissions.");
+        }
+        else if (role is null)
+        {
+            await ReplyAsync($"{Mention} The {roleName} role does not exist");
+        }
+        else
+        {
+            await role.DeleteAsync();
+            await ReplyAsync($"{Mention} The {roleName} role has been deleted!");
+        }
+    }
+}
