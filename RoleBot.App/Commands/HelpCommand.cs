7555934708cbe9e41b03d97a5622a685cc54109b
Christopher Plummer <chrisplummer0194@gmail.com>
2022-01-16 15:52:59 -0500

    Fix command name in Help message

--- RoleBot.App/Commands/HelpCommand.cs
+++ RoleBot.App/Commands/HelpCommand.cs
@@ -10,7 +10,7 @@ public class HelpCommand : CommandBase
         await ReplyAsync($"**The following commands can be used:**{Environment.NewLine}" +
                          $"  • **ping** - Pings the Discord channel{Environment.NewLine}" +
                          $"  • **addrole \"name of role\"** - Adds user to role, and creates role if it doesn't exist{Environment.NewLine}" +
-                         $"  • **removedrole \"name of role\"** - Removes the user from the role, if it exists{Environment.NewLine}" +
+                         $"  • **removerole \"name of role\"** - Removes the user from the role, if it exists{Environment.NewLine}" +
                          "  • **deleterole \"name of role\"** - Deletes the role from the server, if it exists (admin only)");
     }
 }
