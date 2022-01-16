14f1fe4053f55ad125a8e1cd4b3cd9835795b98b
Christopher Plummer <chrisplummer0194@gmail.com>
2022-01-16 13:25:26 -0500

    Add Role Command

--- RoleBot/Commands/AddRoleCommand.cs
+++ RoleBot/Commands/AddRoleCommand.cs
@@ -1,12 +1,28 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 
 namespace RoleBot.Commands;
 
 public class AddRoleCommand : CommandBase
 {
     [Command("addrole")]
-    public async Task HandleCommandAsync(string value)
+    public async Task HandleCommandAsync([Remainder] string roleName)
     {
-        throw new NotImplementedException();
+        var role = Context.Guild.Roles.FirstOrDefault(r => r.Name == roleName);
+        if (role is null)
+        {
+            var newRole = await Context.Guild.CreateRoleAsync(roleName, GuildPermissions.None, null, false, true);
+            await GuildUser.AddRoleAsync(newRole);
+            await ReplyAsync($"{Mention} The {roleName} role has been added!");
+        }
+        else
+        {
+            if (role.Members.Select(m => m.Id).Contains(Context.Message.Author.Id))
+            {
+                await ReplyAsync($"{Mention} You already have the role {roleName}.");
+                return;
+            }
+            await GuildUser.AddRoleAsync(role);
+        }
     }
 }
