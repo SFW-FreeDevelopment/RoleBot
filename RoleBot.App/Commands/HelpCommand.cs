39322788be5527f2c976a219c9b52d85b7cbd8b1
Trevor Reeves <reevest2@gmail.com>
2022-04-05 16:04:59 -0400

    shorten command names

--- RoleBot.App/Commands/HelpCommand.cs
+++ RoleBot.App/Commands/HelpCommand.cs
@@ -9,8 +9,8 @@ public class HelpCommand : CommandBase
     {
         await ReplyAsync($"**The following commands can be used:**{Environment.NewLine}" +
                          $"  • **ping** - Pings the Discord channel{Environment.NewLine}" +
-                         $"  • **addrole \"name of role\"** - Adds user to role, and creates role if it doesn't exist{Environment.NewLine}" +
-                         $"  • **removerole \"name of role\"** - Removes the user from the role, if it exists{Environment.NewLine}" +
-                         "  • **deleterole \"name of role\"** - Deletes the role from the server, if it exists (admin only)");
+                         $"  • **add \"name of role\"** - Adds user to role, and creates role if it doesn't exist{Environment.NewLine}" +
+                         $"  • **remove \"name of role\"** - Removes the user from the role, if it exists{Environment.NewLine}" +
+                         "  • **delete \"name of role\"** - Deletes the role from the server, if it exists (admin only)");
     }
 }
