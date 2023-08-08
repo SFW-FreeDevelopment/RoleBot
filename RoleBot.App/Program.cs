b599513863ac5170d9fb03455274ab91d0ffcc90
Christopher Plummer <chrisplummer0194@gmail.com>
2023-08-08 17:16:27 -0400

    Clean-up on Program.cs

--- RoleBot.App/Program.cs
+++ RoleBot.App/Program.cs
@@ -1,8 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using RoleBot;
-using RoleBot.App;
-
-Console.WriteLine("Hello, World!");
+﻿using RoleBot.App;
 
 await Bot.RunBotAsync();
